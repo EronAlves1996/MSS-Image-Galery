620e4dacc52c7c480d0773fd44df5fd3f6fe293a
Eron Alves <eron.a.silva@capgemini.com>
2023-04-18 00:25:17 -0300

    feat(web): finalize general user home interface

--- apps/web/src/pages/home.tsx
+++ apps/web/src/pages/home.tsx
@@ -7,10 +7,10 @@ export default function Home() {
     <>
       <Navbar />
       <section className="flex flex-col p-5">
-        <h2>Galeria</h2>
-        <section>
+        <h2 className="text-7xl ml-32">Galeria</h2>
+        <section className="flex">
           <Sidebar />
-          <div></div>
+          <div className="flex-grow"></div>
         </section>
       </section>
     </>
@@ -46,12 +46,18 @@ const Navbar = () => {
 const Sidebar = () => (
   <>
     <NavigationMenu.Root>
-      <NavigationMenu.List className="flex flex-col gap-5">
+      <NavigationMenu.List className="flex flex-col gap-5 max-w-7xl">
         <NavigationMenu.Item className="pt-4">
-          <NavigationMenu.Link>Item 1</NavigationMenu.Link>
+          {/*TODO: edit link to point to new album */}
+          <NavigationMenu.Link href="#" className={HOVER_LINK}>
+            Novo album
+          </NavigationMenu.Link>
         </NavigationMenu.Item>
         <NavigationMenu.Item className="border-t-2 border-t-gray-400 pt-4">
-          <NavigationMenu.Link>Item 2</NavigationMenu.Link>
+          {/*TODO: edit link to point to upload new photo*/}
+          <NavigationMenu.Link href="#" className={HOVER_LINK}>
+            Nova foto
+          </NavigationMenu.Link>
         </NavigationMenu.Item>
       </NavigationMenu.List>
     </NavigationMenu.Root>
