f956b63a0fa742b579bceb5d72cd392b6cb1abd9
Eron Alves <eron.a.silva@capgemini.com>
2023-04-18 00:03:17 -0300

    feat(web): finalize navbar

--- apps/web/src/pages/home.tsx
+++ apps/web/src/pages/home.tsx
@@ -1,5 +1,7 @@
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 
+const HOVER_LINK = "hover:font-bold";
+
 export default function Home() {
   return (
     <>
@@ -11,18 +13,24 @@ export default function Home() {
 const Navbar = () => {
   return (
     <NavigationMenu.Root>
-      <NavigationMenu.List>
+      <NavigationMenu.List className="flex justify-between p-5">
         <NavigationMenu.Item>Image Gallery</NavigationMenu.Item>
-        <NavigationMenu.Item>
-          <NavigationMenu.Link href="https://github.com/EronAlves1996/MSS-Image-Galery">
-            Github
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
+        <div className="flex justify-end gap-10">
+          <NavigationMenu.Item className={HOVER_LINK}>
+            <NavigationMenu.Link
+              href="https://github.com/EronAlves1996/MSS-Image-Galery"
+              className="flex gap-2"
+            >
+              <img src={"/Octicons-mark-github.svg"} className="w-6" />
+              <p>Github</p>
+            </NavigationMenu.Link>
+          </NavigationMenu.Item>
 
-        <NavigationMenu.Item>
-          {/*TODO: edit the link to point to logout */}
-          <NavigationMenu.Link href="#">Logout</NavigationMenu.Link>
-        </NavigationMenu.Item>
+          <NavigationMenu.Item className={HOVER_LINK}>
+            {/*TODO: edit the link to point to logout */}
+            <NavigationMenu.Link href="#">Logout</NavigationMenu.Link>
+          </NavigationMenu.Item>
+        </div>
       </NavigationMenu.List>
     </NavigationMenu.Root>
   );
