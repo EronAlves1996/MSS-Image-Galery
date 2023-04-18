e41e162f82dd3ef91b82983173c5a6756f065b59
Eron Alves <eron.a.silva@capgemini.com>
2023-04-18 01:13:40 -0300

    feat(web): make the gallery

--- apps/web/src/pages/home.tsx
+++ apps/web/src/pages/home.tsx
@@ -2,15 +2,38 @@ import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 
 const HOVER_LINK = "hover:font-bold";
 
+//TODO: delete fake thumbs data
+const images = [
+  "12-harold.jpg",
+  "1550001306201.jfif",
+  "500_F_214350091_vk8S4WcEy1vSYozD9S2reb6rVRb8amuL.jpg",
+  "500x500 (1).jpg",
+  "500x500.jpg",
+  "51cxe+2bpvL.jpg",
+  "artworks-000057933559-tr05lk-t500x500.jpg",
+  "download.jfif",
+  "images (1).jfif",
+  "images (2).jfif",
+  "images (3).jfif",
+  "images.jfif",
+  "main-qimg-700d9e110c44a58fb9947cb8ba640c73-lq.jfif",
+  "tumblr_nrut65MIld1rfwfq9o1_500.jpg",
+  "web-dubois-1868-1963-american-educator-editor-and-writer-who-helped-create-the-naacp-national-association-for-the-advancement-of-colored-people-undated-photograph-gett.jpg",
+];
+
 export default function Home() {
   return (
     <>
       <Navbar />
-      <section className="flex flex-col p-5">
+      <section className="flex flex-col p-5 gap-4">
         <h2 className="text-7xl ml-32">Galeria</h2>
-        <section className="flex">
-          <Sidebar />
-          <div className="flex-grow"></div>
+        <section className="flex gap-4">
+          <Sidebar className="flex-shrink-0" />
+          <div className="flex flex-wrap gap-1">
+            {images.map((image) => (
+              <ImageItem imageName={image} />
+            ))}
+          </div>
         </section>
       </section>
     </>
@@ -43,9 +66,9 @@ const Navbar = () => {
   );
 };
 
-const Sidebar = () => (
+const Sidebar = ({ className }: { className: string | undefined }) => (
   <>
-    <NavigationMenu.Root>
+    <NavigationMenu.Root {...{ className }}>
       <NavigationMenu.List className="flex flex-col gap-5 max-w-7xl">
         <NavigationMenu.Item className="pt-4">
           {/*TODO: edit link to point to new album */}
@@ -63,3 +86,11 @@ const Sidebar = () => (
     </NavigationMenu.Root>
   </>
 );
+
+const ImageItem = ({ imageName }: { imageName: string }) => {
+  return (
+    <>
+      <img src={`fakeThumbs/${imageName}`} className="w-48 hover:opacity-60" />
+    </>
+  );
+};
