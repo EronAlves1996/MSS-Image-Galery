8cde21714fb7421c5a6359252ca14d0c8a88d988
Eron Alves <eron.a.silva@capgemini.com>
2023-04-19 00:28:49 -0300

    feat(web): added login element

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -4,35 +4,55 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <section className="p-7">
-      <IndexLogo />
-      <Background />
-      <LoginSlot />
-    </section>
+    <div className="bg-[url('/demo-photo.png')] bg-repeat-x h-[90vh] w-screen relative">
+      <div className="bg-gradient-to-br from-white from-30% to-transparent h-[100%] w-[100%] top-0 left-0 absolute" />
+      <section className="grid grid-rows-[0.5fr_minmax(0,_1fr)] grid-cols-[1fr_min-content]">
+        <IndexLogo className="col-span-2" />
+        <div />
+        <LoginSlot />
+      </section>
+    </div>
   );
 }
 
-const IndexLogo = () => (
-  <div className="z-20">
+const IndexLogo = ({ className }: { className?: string }) => (
+  <div className={`z-20 p-7 ${className}`}>
     <h2 className="text-7xl pl-9">Image Gallery</h2>
     <h3>Todas as suas fotos...</h3>
     <h3>...em um só lugar</h3>
   </div>
 );
 
-const Background = () => (
-  <div className="bg-[url('/demo-photo.png')] bg-repeat-x h-screen w-screen absolute -translate-y-20 -z-10">
-    <div className="bg-gradient-to-br from-white from-30% to-transparent h-[100%] w-[100%]" />
-  </div>
-);
-
 const LoginSlot = () => (
-  <div className="z-20">
-    <form>
-      <label>
-        Nome de Usuário
-        <input type="text" />
-      </label>
+  <div className="z-20 border-gray-600 border-solid border-2 rounded-lg bg-white opacity-80 mr-7 w-[27rem]">
+    <form className="p-7 flex flex-col gap-5">
+      <p className="text-center font-bold">Comece já a aproveitar!!</p>
+      <div className="flex flex-col">
+        <label htmlFor="username">Nome de Usuário</label>
+        <input
+          type="text"
+          id="username"
+          className="username border-gray-500 border-solid border-2 rounded-md bg-white"
+          name="password"
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="password">Senha</label>
+        <input
+          type="password"
+          id="password"
+          className="password border-gray-500 border-solid border-2 rounded-md bg-white"
+          name="password"
+        />
+      </div>
+      <div>
+        <button
+          type="submit"
+          className="border-gray-500 border-solid border-2 bg-slate-200 rounded-md p-1 hover:bg-slate-500 hover:text-white"
+        >
+          Entrar
+        </button>
+      </div>
     </form>
   </div>
 );
