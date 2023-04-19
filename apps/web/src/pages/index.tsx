51d0e7964a55186a520cb78d5d29dd1d5f4f587b
Eron Alves <eron.a.silva@capgemini.com>
2023-04-18 23:24:39 -0300

    feat(web): index page background effect

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -3,5 +3,11 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <></>;
+  return (
+    <section className="flex">
+      <div className="bg-[url('/demo-photo.png')] h-screen w-screen">
+        <div className="bg-gradient-to-bl from-white from-30% to-transparent h-[100%] w-[100%]" />
+      </div>
+    </section>
+  );
 }
