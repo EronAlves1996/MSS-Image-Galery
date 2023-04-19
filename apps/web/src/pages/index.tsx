37a299b0c1149b520e5e826ad9db6a88f26b8611
Eron Alves <eron.a.silva@capgemini.com>
2023-04-18 23:35:16 -0300

    feat(web): introduce appname

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -4,9 +4,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <section className="flex">
-      <div className="bg-[url('/demo-photo.png')] h-screen w-screen">
-        <div className="bg-gradient-to-bl from-white from-30% to-transparent h-[100%] w-[100%]" />
+    <section className="p-7">
+      <div className="z-20 absolute">
+        <h2 className="text-7xl pl-9">Image Gallery</h2>
+        <h3>Todas as suas fotos...</h3>
+        <h3>...em um só lugar</h3>
+      </div>
+      <div className="bg-[url('/demo-photo.png')] bg-repeat-x h-screen w-screen absolute">
+        <div className="bg-gradient-to-br from-white from-30% to-transparent h-[100%] w-[100%]" />
       </div>
     </section>
   );
