d30d68746ccc97609f6227470fd69262e278595d
Eron Alves <eron.a.silva@capgemini.com>
2023-04-21 00:50:05 -0300

    feat(web): successfully installed relay

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,12 +1,36 @@
+import { RelayEnvironmentProvider } from "react-relay";
+import { initRelayEnvironment } from "../RelayEnvironment";
+import { RecordSource } from "relay-runtime";
+import { useMemo, useEffect } from "react";
+import { RelayPageProps } from "../relay-types";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import { NextComponentType, NextPageContext } from "next";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps, router }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+  router,
+}: AppProps<RelayPageProps>) {
   const pathname = router.pathname;
+  const environment = useMemo(initRelayEnvironment, []);
 
-  return <Temporary {...{ Component, pageProps, pathname }} />;
+  useEffect(() => {
+    const store = environment.getStore();
+
+    // Hydrate the store.
+    store.publish(new RecordSource(pageProps.initialRecords));
+
+    // Notify any existing subscribers.
+    store.notify();
+  }, [environment, pageProps.initialRecords]);
+
+  return (
+    <RelayEnvironmentProvider environment={environment}>
+      <Temporary {...{ Component, pageProps, pathname }} />
+    </RelayEnvironmentProvider>
+  );
 }
 
 function Temporary({
