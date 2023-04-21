617690f9865692f02af32c6b1f8aba936a7069c7
Eron Alves <eron.a.silva@capgemini.com>
2023-04-21 00:49:31 -0300

    fix(web): try to separate component

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,14 +1,27 @@
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import { NextComponentType, NextPageContext } from "next";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps, router }: AppProps) {
   const pathname = router.pathname;
 
+  return <Temporary {...{ Component, pageProps, pathname }} />;
+}
+
+function Temporary({
+  pathname,
+  pageProps,
+  Component,
+}: {
+  pathname: string;
+  pageProps: any;
+  Component: NextComponentType<NextPageContext, any, any>;
+}) {
   return (
     <>
       <Navbar {...{ pathname }} />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
